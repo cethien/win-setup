cabe0ca1e89760c4d35fa2124dedcfae2f487ca1
cethien <borislaw.sotnikow@gmx.de>
2024-04-28 16:23:23 +0200

    feat: failsafe, empty config

--- setup.ps1
+++ setup.ps1
@@ -5,7 +5,22 @@ param (
     [string[]]$Profiles
 )
 
-$config = Get-Content "$PSScriptRoot/setup.json" | ConvertFrom-Json
+# check if config.json exists
+$configFile = "$HOME/.win-setup/config.json"
+if (-not (Test-Path $configFile)) {
+    # create configFile with $schema and echo "hello world" action
+    $config = @{
+        "$schema" = "https://raw.githubusercontent.com/cethien/win-setup/main/schemas/config.schema.json"
+        actions   = @(
+            @{
+                script = "echo `"Hello World! please edit $HOME/.win-setup/config.json`""
+            }
+        )
+    } | ConvertTo-Json -Depth 10 | Set-Content $configFile
+    exit
+}
+
+$config = Get-Content "$HOME/.win-setup/config.json" | ConvertFrom-Json
 
 $config.actions | ForEach-Object {
     $_.prepare_script | ForEach-Object { Invoke-Expression $_ }

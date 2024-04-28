7f9726c7bdeed5670a8879da2f4263aa5b47e7d8
cethien <borislaw.sotnikow@gmx.de>
2024-04-28 16:33:58 +0200

    fixes: $schema prop, null

--- setup.ps1
+++ setup.ps1
@@ -8,37 +8,46 @@ param (
 # check if config.json exists
 $configFile = "$HOME/.win-setup/config.json"
 if (-not (Test-Path $configFile)) {
-    # create configFile with $schema and echo "hello world" action
+    # create configFile with property called "$schema" and echo "hello world" action
     $config = @{
-        "$schema" = "https://raw.githubusercontent.com/cethien/win-setup/main/schemas/config.schema.json"
-        actions   = @(
+        "`$schema" = "https://raw.githubusercontent.com/cethien/win-setup/main/config.schema.json"
+        actions    = @(
             @{
-                script = "echo `"Hello World! please edit $HOME/.win-setup/config.json`""
+                script = "echo `"Hello World! please edit $HOME\.win-setup\config.json`""
             }
         )
     } | ConvertTo-Json -Depth 10 | Set-Content $configFile
-    exit
 }
 
 $config = Get-Content "$HOME/.win-setup/config.json" | ConvertFrom-Json
 
 $config.actions | ForEach-Object {
-    $_.prepare_script | ForEach-Object { Invoke-Expression $_ }
+    if ($_.prepare_script -ne $null) {
+        $_.prepare_script | ForEach-Object { Invoke-Expression $_ }
+    }
 
-    Invoke-Expression $_.script
+    if ($_.script -ne $null) {
+        Invoke-Expression $_.script
+    }
 
-    $_.winget_packages | ForEach-Object {
-        $cmd = "winget install --accept-source-agreements --accept-package-agreements --source winget --Id $($_.id) $($_.install_flags -join " ")"
-        $cmd | Invoke-Expression
+    if ($_.winget_packages -ne $null) {
+        $_.winget_packages | ForEach-Object {
+            $cmd = "winget install --accept-source-agreements --accept-package-agreements --source winget --Id $($_.id) $($_.install_flags -join " ")"
+            $cmd | Invoke-Expression
 
-        if ($_.exclude_from_updatefile -ne $true) {
-            Add-Content $env:USERPROFILE/.wingetupdate "$($_.id)`n"
+            if ($_.exclude_from_updatefile -ne $true) {
+                Add-Content $env:USERPROFILE/.wingetupdate "$($_.id)`n"
+            }
         }
     }
 
-    $_.pwsh_modules | ForEach-Object {
-        Install-Module $_
+    if ($_.pwsh_modules -ne $null) {
+        $_.pwsh_modules | ForEach-Object {
+            Install-Module $_
+        }
     }
 
-    Invoke-Expression $_.post_install_script
+    if ($_.post_install_script -ne $null) {
+        Invoke-Expression $_.post_install_script
+    }
 }

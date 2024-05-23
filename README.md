e1c4da581890c5f3f6ec6a1ac7330591ab2c3e21
cethien <borislaw.sotnikow@gmx.de>
2024-05-23 20:53:05 +0200

    chore: init

---
+++ README.md
@@ -0,0 +1,7 @@
+# WSL setup script
+
+usage:
+
+```bash
+ sh <(curl -fsSL "https://raw.githubusercontent.com/cethien/setup/main/setup.sh")
+```
---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+## setup ubuntu/debian in wsl
+
+# install nala
+sudo apt update &&
+    sudo apt install -y nala
+
+# update distro
+sudo nala update &&
+    sudo nala upgrade -y
+
+# nix + home manager
+sh <(curl -fsSL https://nixos.org/nix/install) --no-daemon &&
+    echo "experimental-features = nix-command flakes" >> $XDG_CONFIG_HOME/nix/nix.conf
+    mv "$HOME"/.bashrc "$HOME"/.bashrc_default &&
+    mv "$HOME"/.profile "$HOME"/.profile_default &&
+    . "$HOME"/.nix-profile/etc/profile.d/nix.sh &&
+    nix build .config/home-manager#homeConfigurations.cethien.activationPackage &&
+    result/activate &&
+    home-manager switch &&
+    rm -rf result
+
+# remove sudo pw prompt
+echo "$USER ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/"$USER" >/dev/null
+
+sudo reboot
